bb459304cb140f6b7d3e222a9b8543394ee89423
Akezhan Mussa <ake.mussa@gmail.com>
2021-03-20 22:42:21 +0600

    Modifed gitignore, vision and detector

--- ds/services/detector.py
+++ ds/services/detector.py
@@ -2,6 +2,13 @@ import enum
 import cv2
 import face_recognition
 import numpy as np
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.models import load_model
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
+
+
+faceNet = None
+maskNet = None
 
 
 class DetectionType(enum.Enum):
@@ -27,3 +34,75 @@ def face_detect(frame, known_face_encodings, known_face_names):
         face_names.append(name)
 
     return face_locations, face_names
+
+
+def setup_mask_models(model_path: str, prototxt_path, weights_path):
+    global faceNet, maskNet
+
+    if faceNet is None:
+        faceNet = cv2.dnn.readNet(prototxt_path, weights_path)
+
+    if maskNet is None:
+        maskNet = load_model(model_path)
+
+
+def detect_and_predict_mask(frame, threshold_confidence=0.5):
+    global faceNet, maskNet
+
+    if faceNet is None or maskNet is None:
+        return
+
+    (h, w) = frame.shape[:2]
+    blob = cv2.dnn.blobFromImage(frame, 1.0, (300, 300),
+                                 (104.0, 177.0, 123.0))
+
+    faceNet.setInput(blob)
+    detections = faceNet.forward()
+
+    faces = []
+    locs = []
+    preds = []
+
+    # loop over the detections
+    for i in range(0, detections.shape[2]):
+        # extract the confidence (i.e., probability) associated with
+        # the detection
+        confidence = detections[0, 0, i, 2]
+
+        # filter out weak detections by ensuring the confidence is
+        # greater than the minimum confidence
+        if confidence > threshold_confidence:
+            # compute the (x, y)-coordinates of the bounding box for
+            # the object
+            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+            (startX, startY, endX, endY) = box.astype("int")
+
+            # ensure the bounding boxes fall within the dimensions of
+            # the frame
+            (startX, startY) = (max(0, startX), max(0, startY))
+            (endX, endY) = (min(w - 1, endX), min(h - 1, endY))
+
+            # extract the face ROI, convert it from BGR to RGB channel
+            # ordering, resize it to 224x224, and preprocess it
+            face = frame[startY:endY, startX:endX]
+            face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
+            face = cv2.resize(face, (224, 224))
+            face = img_to_array(face)
+            face = preprocess_input(face)
+
+            # add the face and bounding boxes to their respective
+            # lists
+            faces.append(face)
+            locs.append((startX, startY, endX, endY))
+
+    # only make a predictions if at least one face was detected
+    if len(faces) > 0:
+        # for faster inference we'll make batch predictions on *all*
+        # faces at the same time rather than one-by-one predictions
+        # in the above `for` loop
+        faces = np.array(faces, dtype="float32")
+        preds = maskNet.predict(faces, batch_size=32)
+
+    # return a 2-tuple of the face locations and their corresponding
+    # locations
+    return locs, preds
--- ds/services/vision.py
+++ ds/services/vision.py
@@ -15,3 +15,19 @@ def display_with(frame, face_locations, face_names):
         cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
 
     cv2.imshow('Video', frame)
+
+
+def display_mask_with(frame, locs, preds):
+    for (box, pred) in zip(locs, preds):
+        (startX, startY, endX, endY) = box
+        (mask, withoutMask) = pred
+
+        label = "Mask" if mask > withoutMask else "No Mask"
+        color = (0, 255, 0) if label == "Mask" else (0, 0, 255)
+        label = "{}: {:.2f}%".format(label, max(mask, withoutMask) * 100)
+
+        cv2.putText(frame, label, (startX, startY - 10),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
+        cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
+
+    cv2.imshow("Frame", frame)
