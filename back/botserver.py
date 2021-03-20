a6c6650ce1673c55e7ef1b483db56d794f19f0ec
seilkhanovkkk <seil.kuka@gmail.com>
2021-03-21 01:43:06 +0600

    added admin

--- back/botserver.py
+++ back/botserver.py
@@ -1,6 +1,9 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import pathlib
+import requests
+import json
 
+db = {}
 
 def start(update, callback):
     bot = callback.bot
@@ -8,8 +11,8 @@ def start(update, callback):
     bot.send_message(chat_id=chat_id, text="Hi")
     bot.send_message(chat_id=chat_id, text="Send please your photo")
 
-
 def echo(update, callback):
+    global db
     bot = callback.bot
     chat_id = update.message.chat_id
     image = update.message.photo[-1]
@@ -19,6 +22,11 @@ def echo(update, callback):
     if not pathlib.Path(faces_dir).exists():
         pathlib.Path(faces_dir).mkdir()
 
+    if chat_id not in  db: 
+        db[chat_id] = image.file_id
+        with open("db.json", "w") as f:
+            json.dump(db, f)
+
     path = f'{faces_dir}/{chat_id}.jpg'
     newFile = bot.getFile(image.file_id)
     newFile.download(path)
@@ -26,6 +34,9 @@ def echo(update, callback):
 
 
 def main():
+    global db
+    with open("db.json") as f:
+        db = json.load(f)
     updater = Updater('1515946405:AAE96PUEIZr24Rnn_y-0RxYctO9acO2ukFw')
     dp = updater.dispatcher
 
--- back/postserver.py
+++ back/postserver.py
@@ -1,18 +1,39 @@
 from flask import Flask, request
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-
+import botserver
+import json
 app = Flask(__name__)
+bot = Updater('1515946405:AAE96PUEIZr24Rnn_y-0RxYctO9acO2ukFw').bot
+admin_id = '359715487'
 
 
 @app.route('/nomask', methods=['POST'])
 def index():
     request_data = request.get_json()
-    print(request_data)
     chat_id = request_data['id']
-    bot = Updater('1515946405:AAE96PUEIZr24Rnn_y-0RxYctO9acO2ukFw').bot
-    bot.send_message(chat_id=chat_id, text="You are not wearing mask 🤬")
-    return "Hello World"
+    location = request_data['location']
 
+    bot.send_message(chat_id=chat_id, text="You are not wearing mask 🤬")
+    bot.send_message(chat_id=chat_id, text="I know your location, shit")
+    bot.send_message(chat_id=chat_id, text=location)
+
+    return "Hello Pidor"
+
+@app.route('/admin', methods=['POST'])
+def admin():
+    request_data = request.get_json()
+    chat_id = request_data['id']
+    location = request_data['location']
+
+    with open("db.json") as f:
+        db = json.load(f)
+    image_id = db[chat_id]
+
+    bot.send_message(chat_id=admin_id, text="This bastard does not wear mask")
+    bot.send_photo(chat_id=admin_id, photo=image_id)
+    bot.send_message(chat_id=admin_id, text="Its location: ")
+    bot.send_message(chat_id=admin_id, text=location)
+    return "Hello Pidor"
 
 if __name__ == "__main__":
     app.run(debug=True)
