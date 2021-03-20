cc72c1a2f614ea0d4a3f4da624d4bbbe8a5fa257
Akezhan Mussa <ake.mussa@gmail.com>
2021-03-20 22:40:48 +0600

    Added detector for mask recognition

--- app.py
+++ app.py
@@ -1,16 +1,11 @@
 import ds
 
 
-def app():
+def face_app():
     known_face_names, known_face_encodings = ds.services.face_encoder.generate_image_encodings('faces')
-
-    print(f'Known face names is {known_face_names}')
 
     with ds.services.camera.Camera() as camera:
         for frame in camera.read_frames(no_limit=True):
-            print(f'Frame is {frame}')
-            print(f'{known_face_encodings}')
-            print(f'{known_face_names}')
             face_locations, face_names = ds.services.detector.face_detect(
                 frame,
                 known_face_encodings,
@@ -19,5 +14,19 @@ def app():
             ds.services.vision.display_with(frame, face_locations, face_names)
 
 
+def mask_app():
+
+    model_path = 'models/mask_detector.model'
+    prototxt_path = 'models/deploy.prototxt'
+    weights_path = 'models/res10_300x300_ssd_iter_140000.caffemodel'
+
+    ds.services.detector.setup_mask_models(model_path, prototxt_path, weights_path)
+
+    with ds.services.camera.Camera() as camera:
+        for frame in camera.read_frames(no_limit=True):
+            locs, preds = ds.services.detector.detect_and_predict_mask(frame)
+            ds.services.vision.display_mask_with(frame, locs, preds)
+
+
 if __name__ == '__main__':
-    app()
+    mask_app()
